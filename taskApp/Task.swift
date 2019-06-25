c70b207ab4840bdb7cd60c32697f9adda77e67a1
Takuya Hatakeyama <tk.shsf@gmail.com>
2019-06-25 22:55:38 +0900

    課題の手前まで完成

---
+++ taskApp/Task.swift
@@ -0,0 +1,30 @@
+//
+//  Task.swift
+//  
+//
+//  Created by Takuya Hatakeyama on 2019/06/24.
+//
+
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    @objc dynamic var id = 0
+    
+    // タイトル
+    @objc dynamic var title = ""
+    
+    // 内容
+    @objc dynamic var contents = ""
+    
+    /// 日時
+    @objc dynamic var date = Date()
+    
+    /**
+     id をプライマリーキーとして設定
+     */
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
